a56ac52ea9186fa436d8a320467b2d9a0bbe4bb1
Austin Lucas <aclucas196@gmail.com>
2021-07-14 09:54:43 -0500

    Update ETL Interface

--- Interfaces/IEtl.cs
+++ Interfaces/IEtl.cs
@@ -1,8 +1,18 @@
+using System.Collections.Generic;
+using System.Dynamic;
+
 namespace etl.Interfaces
 {
     public interface IEtl
     {
-         public void DoWork();
-         public bool ShouldRun();
+        public void DoWork();
+        public bool ShouldRun();
+        public void InsertData(string insert_proc_name);
+        public void DeleteData(string delete_proc_name);
+        public void UpdateData(string update_proc_name);
+        public void LoadLinxTable(List<ExpandoObject> linxData);
+        public List<ExpandoObject> GetLinxData();
+        public List<ExpandoObject> GetAllFromDB();
+
     }
 }
